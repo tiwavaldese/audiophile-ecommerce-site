f033f815894d16553179416e5631be326ef3b7df
tiwavaldese <tiwaval7@gmail.com>
2024-05-29 20:19:51 +0100

    adding a nav bar in the home component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Home from './components/Home'
 
 const App = () => {
   return (
     <div>
-      <p className=" text-lg text-[blue] font-bold">Hello, Tailwind CSS!</p>
+      <Home/>
     </div>
   )
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import Shape from '../assets/Shape.png'
 
 const Home = () => {
   return (
     <div>
-      
+      <nav>
+        <div>audiophile</div>
+        <div>
+            <ul>
+                <li>HOME</li>
+                <li>HEADPHONES</li>
+                <li>SPEAKERS</li>
+                <li>EARPHONES</li>
+            </ul>
+        </div>
+        <div>
+          <img src={Shape}  alt="Shape" />  
+        </div>     
+ </nav>
     </div>
   )
 }
