7dab71d838d51633f85a97f11d53392f02a4d00a
tiwavaldese <tiwaval7@gmail.com>
2024-06-01 11:00:15 +0100

    update the earphone component

--- src/components/Earphones.jsx
+++ src/components/Earphones.jsx
@@ -1,10 +1,63 @@
 import React from 'react'
 import NavBar from './NavBar'
+import headphone from '../assets/Headphone.png'
+import speaker from '../assets/Speaker.png';
+import earphone from '../assets/Earphone.png';
+import bitmap3 from '../assets/Bitmap-3.png'
+import Footer from './Footer';
+import earphone1 from '../assets/Earphone1.png'
 
 const Earphones = () => {
   return (
     <div>
       <NavBar/>
+      <div className='flex p-4 bg-[black]  justify-center h-custom-750 '>
+        <h2 className='text-white text-3xl font-bold mb-9'>EARPHONES</h2>
+      </div>
+      <div className='flex max-w-4xl justify-evenly mx-auto my-24'>
+        <div className='flex-1'>
+          <img src={earphone1}/>
+        </div>
+        <div className='flex-1 ml-24 mt-12'>
+            <span className='text-sm text-[orange] tracking-widest' >NEW PRODUCT</span>
+            <h2 className='font-bold text-5xl my-5 '>YX1 WIRELESS <br/> EARPHONES</h2>
+            <p className='mb-5  '>Tailor your listening experience with bespoke dynamic drivers from the new YX1 Wireless Earphones. Enjoy incredible high-fidelity sound even in noisy environments with its active noise cancellation feature.</p>
+            <button className='bg-orange-bg p-3 text-xs text-white '>SEE PRODUCT</button>
+        </div>
+      </div >
+      <div className='flex justify-evenly my-24 max-w-5xl mx-auto  '>
+      <div className='relative bg-light-gray h-40 w-64 mt-12 '>
+        <img className='absolute top-0 left-1/2 transform -translate-x-1/2 -translate-y-1/2' src={headphone}/>
+      <div className='text-center mt-12 pt-11'>
+        <h2 className='font-bold'>HEADPHONES</h2>
+        <p className='text-xs'>SHOP <span></span> </p>
+      </div>
+      </div>
+      <div className='relative bg-light-gray h-40 w-64 mt-12'>
+      <img className='absolute top-0 left-1/2 transform -translate-x-1/2 -translate-y-1/2' src={speaker} />
+      <div className='text-center mt-12 pt-11'>
+      <h2 className='font-bold'>SPEAKERS</h2>
+      <p className='text-xs'>SHOP <span></span> </p>
+      </div>
+      </div>
+      <div className='relative bg-light-gray h-40 w-64 mt-12'>
+      <img className='absolute top-0 left-1/2 transform -translate-x-1/2 -translate-y-1/2' src={earphone} />
+      <div className='text-center mt-12 pt-11'>
+      <h2 className='font-bold'>EARPHONES</h2>
+      <p className='text-xs'>SHOP <span></span> </p>
+      </div>
+      </div>
+     </div>
+     <div className='flex mx-auto max-w-4xl my-24 '>
+      <div className='flex-1 mt-24'>
+        <h2 className='text-4xl mb-5'>BRINING YOU THE<br/> <span className='text-[orange]'>BEST</span> AUDIO GEAR</h2>
+        <p className='text-xs leading-relaxed'>Located at the heart of New York City, Audiophile is the premier <br/> store for high end headphones, earphones, speakers, and audio <br/> accessories. We have a large showroom and luxury <br/> demonstration rooms available for you to browse and<br/> experience a wide range of our products. Stop by our store to<br/> meet some of the fantastic people who make Audiophile the <br/>best place to buy your portable audio equipment.</p>
+      </div>
+      <div className='flex-1'>
+        <img src={bitmap3} />
+      </div>
+     </div>
+     <Footer/>
     </div>
   )
 }
