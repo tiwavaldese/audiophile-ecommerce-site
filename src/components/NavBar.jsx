03a5b942bfa6f980bdf9f9878ed69dc9422853fa
tiwavaldese <tiwaval7@gmail.com>
2024-06-04 09:25:36 +0100

    making the navbar component to be responsive

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,22 +1,29 @@
-import React from 'react'
+import React, {useState} from 'react'
 import shape from '../assets/Shape.png'
 import { Link } from 'react-router-dom'
 
 const NavBar = () => {
+  const[menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu =()=>{
+    setMenuOpen(!menuOpen)
+  }
+
   return (
     <div className=' bg-[black]'>
-       <nav className='p-12 mx-auto items-center flex justify-between  text-[white]'>
-        <div className='ml-28 font-bold'>audiophile</div>
-        <div>
-            <ul className='flex gap-6 ml-24'>
+       <nav className='p-12 mx-auto  flex justify-between  text-[white]'>
+        <button onClick={toggleMenu} className='text-white lg:hidden'>☰</button>
+        <div className='ml-0 lg:ml-28 font-bold'>audiophile</div>
+        <div className={`absolute top-16 left-0 lg:static  lg:w-auto  w-full lg:flex gap-6 ${menuOpen ? 'block' : 'hidden'} `} >
+            <ul className='flex flex-col lg:flex-row gap-6 ml-24'>
                 <li><Link to='/'>HOME</Link></li>
                 <li className='text-[orange]'><Link to='/headphones'>HEADPHONES</Link></li>
                 <li><Link to='/speakers'>SPEAKERS</Link></li>
                 <li><Link to='/earphones'>EARPHONES</Link></li>
             </ul>
         </div>
-        <div className='ml-24'>
-            <img className='text-[black] ' src={shape} alt='shape' />
+        <div className=' ml-10 md:ml-80 lg:ml-24'>
+            <img className='' src={shape} alt='shape' />
         </div>  
         <hr/>   
  </nav>
