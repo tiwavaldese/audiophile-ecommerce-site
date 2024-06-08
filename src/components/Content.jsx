94b5c7a53daba604e25ec6e37034347f445706b2
tiwavaldese <tiwaval7@gmail.com>
2024-06-08 07:47:05 +0100

    modify and update few files

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -11,8 +11,9 @@ import { Link } from 'react-router-dom';
 function Content() {
   return (
     <div>
-     <div className='flex flex-col md:flex-row lg:flex-row items-center justify-center md:justify-evenly lg:justify-evenly my-24 max-w-5xl mx-auto  '>
-      <div className='relative bg-light-gray h-40 w-64 mt-12 '>
+     <div className='flex flex-col md:flex-row lg:flex-row items-center 
+      justify-center md:justify-evenly lg:justify-evenly my-24 max-w-7xl md:max-w-4xl lg:max-w-5xl mx-auto  '>
+      <div className='relative bg-light-gray mb-9 md:mb-0 lg:mb-0 h-40 w-64 mt-12 '>
         <img className='absolute top-0 left-1/2 transform -translate-x-1/2 -translate-y-1/2' src={headphone}/>
       <div className='text-center mt-12 pt-11'>
         <h2 className='font-bold'>HEADPHONES</h2>
@@ -23,7 +24,7 @@ function Content() {
          </Link>
       </div>
       </div>
-      <div className='relative bg-light-gray h-40  w-64 mt-12'>
+      <div className='relative bg-light-gray mb-7 md:mb-0 lg:mb-0 h-40  w-64 mt-12'>
       <img className='absolute top-0 left-1/2 transform -translate-x-1/2 -translate-y-1/2' src={speaker} />
       <div className='text-center mt-12 pt-11'>
       <h2 className='font-bold'>SPEAKERS</h2>
@@ -46,18 +47,18 @@ function Content() {
       </div>
       </div>
      </div>
-     <div className='flex justify-evenly bg-oranger-bg mx-auto max-w-4xl h-400'>
+     <div className='flex flex-col lg:flex-row items-center justify-center lg:justify-evenly bg-oranger-bg mx-auto max-w-2xl lg:max-w-4xl h-700 lg:h-400'>
      <div className=' w-600 h-400 '>
         <img className='mt-28 mb-0 h-300' src={speaker} alt='speaker' />
       </div>
-      <div className='mt-32'>
+      <div className=' mt-12 flex flex-col lg:mt-32 items-center lg:items-start'>
         <h2 className='text-c-white  text-3xl font-bold'>ZX9 <br/> SPEAKER</h2>
-        <p className='text-c-white my-5'>Upgrade to premium speakers that are <br/> phenomenally built to deliver truly remarkable <br/> sound.</p>
+        <p className='text-c-white  my-5'>Upgrade to premium speakers that are <br/> phenomenally built to deliver truly remarkable <br/> sound.</p>
         <Link to='/speakers1'><button className='bg-m-dark p-3 w-50 h-9 my-auto text-xs text-c-white mb-10'>SEE PRODUCT</button></Link>
       </div>
      </div>
-     <div className='flex mx-auto max-w-4xl my-12 relative'>
-      <div className='absolute  top-24 left-2 pl-80 transform -translate-x-1/2 -translate-y-1/2'>
+     <div className='flex mx-auto max-w-2xl lg:max-w-4xl my-12 relative'>
+      <div className='absolute  top-10 md:top-24 lg:top-24 left-2 pl-36 md:pl-80 lg:pl-80 transform -translate-x-1/2 -translate-y-1/2'>
         <h2 className='font-bold text-2xl mb-3'>ZX7 SPEAKER</h2>
         <Link to='/speakers2'><button className='bg-m-dark p-3 w-50 h-9 my-auto text-xs text-c-white'>SEE PRODUCT</button></Link>
       </div>
@@ -65,7 +66,7 @@ function Content() {
         <img src={bitmap1} />
       </div>
      </div>
-     <div className='flex mx-auto max-w-4xl my-12 gap-4'>
+     <div className='flex flex-col md:flex-row lg:flex-row mx-auto  max-w-2xl lg:max-w-4xl my-12 gap-4'>
       <div className='flex-1'>
         <img src={bitmap2} />
       </div>
@@ -74,12 +75,12 @@ function Content() {
         <Link to='/earphones1'><button className='bg-m-dark p-3 w-15 h-9  text-xs text-c-white'>SEE PRODUCT</button></Link>
       </div>
      </div>
-     <div className='flex mx-auto max-w-4xl my-24 '>
-      <div className='flex-1 mt-24'>
+     <div className='flex flex-col lg:flex-row mx-auto max-w-3xl items-center lg:max-w-4xl my-24 '>
+      <div className='flex-1 mt-24 order-2 lg:order-1'>
         <h2 className='text-4xl mb-5'>BRINING YOU THE<br/> <span className='text-[orange]'>BEST</span> AUDIO GEAR</h2>
         <p className='text-xs leading-relaxed'>Located at the heart of New York City, Audiophile is the premier <br/> store for high end headphones, earphones, speakers, and audio <br/> accessories. We have a large showroom and luxury <br/> demonstration rooms available for you to browse and<br/> experience a wide range of our products. Stop by our store to<br/> meet some of the fantastic people who make Audiophile the <br/>best place to buy your portable audio equipment.</p>
       </div>
-      <div className='flex-1'>
+      <div className='flex-1 order-1 lg:order-2'>
         <img src={bitmap3} />
       </div>
      </div>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,20 +6,20 @@ const Footer = () => {
   return (
 
       <div className=' bg-[black]'>
-       <nav className='p-12 mx-auto  flex flex-col md:flex-row lg:flex-row justify-evenly text-[white]'>
-        <div className='ml-12 '>
-            <h2 className=' font-bold'>audiophile</h2>
-        <p className='my-5 text-white-50'>Audiophile is an all in one stop to fulfill your audio needs. We're a small team <br/>of music lovers and sound specialists who are devoted to helping you get the <br/>most out of personal audio. Come and visit our demo facility - we’re open 7<br/> days a week.</p>
-        <p className='text-white-50'>Copyright 2021. All Rights Reserved</p>
+       <nav className='p-12 mx-auto  flex flex-col  lg:flex-row justify-evenly text-[white]'>
+        <div className='ml-0 items-center lg:ml-12 '>
+            <div className='order-1'><h2 className=' font-bold order-1'>audiophile</h2></div>
+        <div className='order-3'><p className='my-5 text-white-50 order-3'>Audiophile is an all in one stop to fulfill your audio needs. We're a small team of music lovers and sound specialists who are devoted to helping you get the most out of personal audio. Come and visit our demo facility - we’re open 7 days a week.</p></div>
+        <div className='order-4'><p className='text-white-50 order-4' >Copyright 2021. All Rights Reserved</p></div>
         </div>
-        <div>
-            <ul className='flex gap-6 ml-12'>
+        <div className='order-2'>
+            <ul className='flex flex-col items-center md:flex-row lg:flex-row gap-6 ml-0 lg:ml-12'>
             <li><Link to='/'>HOME</Link></li>
                 <li className='text-[orange]'><Link to='/headphones'>HEADPHONES</Link></li>
                 <li><Link to='/speakers'>SPEAKERS</Link></li>
                 <li><Link to='/earphones'>EARPHONES</Link></li>
             </ul>
-            <div className='flex gap-3 justify-end mt-24'>
+            <div className='flex gap-3 justify-center lg:justify-end mt-24 order-5'>
                 <span className='bg-[orange] text-black p-1' ><FaFacebookF/></span>
                 <FaTwitter />
                 <FaInstagram/>
